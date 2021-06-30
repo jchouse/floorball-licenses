05dfa31bac727064b6c721742cd09fd6f25653e7
Sergii Syrkin <sergii.syrkin@slidepresenter.com>
2021-06-30 11:26:51 +0200

    DB script

--- src/db/db_migration.js
+++ src/db/db_migration.js
@@ -3,12 +3,22 @@
 
 const fs = require('fs');
 
-const filename = './floorball-b5840-export.json';
+const filename = './floorball-prod-export.json';
 const rawdata = fs.readFileSync(filename);
 
 const data = JSON.parse(rawdata);
 
 // do smth with data here
+
+const playersKeys = Object.keys(data.players);
+
+playersKeys.forEach(pk => {
+  const player = data.players[pk];
+
+  if (player && (player.endActivationDate === 1627682400000 || player.endActivationDate === 1630360800000)) {
+    player.endActivationDate = 1625004000000;
+  }
+});
 
 // save data here
 fs.writeFileSync('./db_conveted.json', JSON.stringify(data));
