a9ccde4e28d9b31bd814c483876fea9cd8bd57cb
Sergii [JCHouse] Syrkin <s.syrkin@gmail.com>
2023-03-03 13:38:03 +0100

    Fix lint worning

--- src/components/Floorball/Floorball.tsx
+++ src/components/Floorball/Floorball.tsx
@@ -16,7 +16,6 @@ import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
